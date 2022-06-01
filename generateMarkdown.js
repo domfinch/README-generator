9bb41c3028f1a3b5d7e92e1284218e760d52d20a
domfinch <finch.163@buckeyemail.osu.edu>
2022-06-01 15:50:15 -0400

    updated files

---
+++ generateMarkdown.js
@@ -0,0 +1,39 @@
+function generateMarkdown(promptResponse) {
+    var content = `# ${promptResponse.projectTitle}
+    
+    # License
+    ![${promptResponse.license}](https://img.shields.io/badge/license-${promptResponse.license}-red)
+  
+    ### Table of Contents
+    * [Description](#Description)
+    * [Usage](#Usage)
+    * [Installation](#Installation)
+    * [Contributing](#Contributing)
+    * [Testing](#Testing)
+    
+    
+    ## Description
+    ${promptResponse.description}
+  
+    # Usage
+    ${promptResponse.usage}
+  
+    # Installation
+    ${promptResponse.installation-process}
+  
+    # Contributing
+    ${promptResponse.contributing}
+  
+    # Testing
+    ${promptResponse.tests}
+  
+    ### Questions
+    If you have any questions you can contact me via: [Github](https://www.github.com/${promptResponse.userNameGithub}) [Email Me](mailto:${promptResponse.userEmail})
+    
+    `
+    return content;
+  
+  };
+  
+  module.exports = generateMarkdown;
+  
